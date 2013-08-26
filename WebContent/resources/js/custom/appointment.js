fb875548cf3790d37de587df5470404b8103b65e
shyam <shyam.shyre@gmail.com>
2013-08-26 23:37:58 +0530

    inital commit

--- WebContent/resources/js/custom/appointment.js
+++ WebContent/resources/js/custom/appointment.js
@@ -23,101 +23,169 @@ function servicesTaskList(serviceTaskId,skillId,skillName,boothType,facilityId,f
 	this.serviceTaskOrder= ko.observable(serviceTaskOrder);
 	this.serviceTaskName= ko.observable(serviceTaskName);
 	this.deleteFlag= ko.observable(deleteFlag);
-
+	
 }
 
+	function ApptSchedule(appointmentName,appointmentDate,agentId,serviceId,notes,apptSourceChannel,apptSourceDt,apptSourceClientName)
+	{
+	this.appointmentName = ko.observable(appointmentName);
+	this.appointmentDate = ko.observable(appointmentDate);	
+	this.agentId = ko.observable(agentId);	
+	this.serviceId = ko.observable(serviceId);	
+	this.notes = ko.observable(notes);	
+	this.apptSourceChannel = ko.observable(apptSourceChannel);	
+	this.apptSourceDt = ko.observable(apptSourceDt);	
+	this.apptSourceClientName = ko.observable(apptSourceClientName);
+	}
 
 function Agent(agentName,agentId){
 	this.agentName = ko.observable(agentName);
 	this.agentId = ko.observable(agentId);
 }
 
+$(function () {
+    $.fn.serializeObject = function()
+    {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function(i,fd) {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+               // o[this.name].push(this.value || '');
+                } else {
+                	  if(fd.name == "appointmentDate"){
+                          var seldate= this.value;
+                          var seldatetime = new Date(seldate).getTime();
+                          o[this.name] = seldatetime;
+                          return;
+                          }
+                o[this.name] = this.value || '';
+                }
+            
+        });
+        return o;
+    }
+});
+
+
+
+
 var apptViewModel={
 		apptagents: ko.observableArray([]),
-		apptservices   :ko.observableArray([]),		
-		selectTmpl		: ko.observable(""),	
+		apptservices   :ko.observableArray([]),
+		apptschedule   :ko.observableArray([]),
+		selectTmpl		: ko.observable(''),	
 		loaddata:function(appt){
 			var xhr = $.ajax({
-				type: "GET",
-				url: "http://localhost:8080/ScreenTest/output/apptservices.json",
-				//data: "name=John&location=Boston",
-				success: function(data){
-					var obj= ko.utils.arrayMap(data,function(item){
-						return new serviceList(item.serviceName,item.serviceDuration,item.serviceDefaultCost,item.serviceId,item.serviceTaskList); 
-					});
-					apptservices= obj;
-				}
+			    type: "GET",
+			    url: "http://localhost:8080/ScreenTest/output/apptservices.json",
+			    //data: "name=John&location=Boston",
+			    success: function(data){
+			      var obj= ko.utils.arrayMap(data,function(item){
+			    	   return new serviceList(item.serviceName,item.serviceDuration,item.serviceDefaultCost,item.serviceId,item.serviceTaskList); 
+			       });
+			      apptservices= obj;
+					    }
 			});
 		},
 		loadAgents:function(data,event){
 			$( "#datepicker" ).datepicker();
 			var serviceid_id=ko.toJSON(data.serviceId);
 			var xhr = $.ajax({
-				type: "GET",
-				url: "http://localhost:8080/ScreenTest/output/apptagents.json",
-				//url: "http://localhost:8080/ScreenTest/output/apptagents.json/"+serviceid_id,
-				//data: "name=John&location=Boston",
-				success: function(data){
-					var obj= ko.utils.arrayMap(data,function(item){
-						return new Agent(item.agentName,item.agentId); 
-					});
-					apptagents= obj;
-					// alert(ko.toJSON(apptagents));
-				}
+			    type: "GET",
+			    url: "http://localhost:8080/ScreenTest/output/apptagents.json",
+			    //url: "http://localhost:8080/ScreenTest/output/apptagents.json/"+serviceid_id,
+			    //data: "name=John&location=Boston",
+			    success: function(data){
+			      var obj= ko.utils.arrayMap(data,function(item){
+			    	   return new Agent(item.agentName,item.agentId); 
+			       });
+			      apptagents= obj;
+			     // alert(ko.toJSON(apptagents));
+			    }
 			});
-
+			
 			apptViewModel.setTmpl('apptserviceProvider');
 		},
 		loadSchedule:function(data,event){
 			var agentId=ko.toJSON(data.agentId);
 			//alert(agentId);
 			var xhr = $.ajax({
-				type: "GET",
-				url: "http://localhost:8080/ScreenTest/output/apptagents.json",
-				//url: "http://localhost:8080/ScreenTest/output/apptagents.json/"+serviceid_id,
-				//data: "name=John&location=Boston",
-				success: function(data){
-					var obj= ko.utils.arrayMap(data,function(item){
-						return new Agent(item.agentName,item.agentId); 
-					});
-					apptagents= obj;
-					// alert(ko.toJSON(apptagents));
-				}
+			    type: "GET",
+			    url: "http://localhost:8080/ScreenTest/output/apptschedule.json",
+			    //url: "http://localhost:8080/ScreenTest/output/apptagents.json/"+serviceid_id,
+			    //data: "name=John&location=Boston",
+			    success: function(data){
+			      var obj= ko.utils.arrayMap(data,function(item){
+			    	   return new ApptSchedule(item.appointmentName,item.appointmentDate,item.agentId,item.serviceId,item.notes,item.apptSourceChannel,item.apptSourceDt,item.apptSourceClientName); 
+			       });
+			      apptschedule= obj;
+			      alert(ko.toJSON(apptschedule));
+			    }
 			});
-
 			apptViewModel.setTmpl('apptdateTime');
-			$( "#datepicker" ).datepicker();
+			alert("initalized");
+	  		var currentDate = $("#datepicker").datepicker({
+			     dateFormat: "yy-mm-dd", 
+			     onSelect: function( selectedDate ){
+			    	 alert("Selected"+selectedDate);
+			    	// $.data(seldate,'seldate',selectedDate);
+					  //$.getJSON("<%=request.getContextPath()%>/rest/appointment/getAgentsAppointment/"+moddate.toString(),
+			    	 var xhr = $.ajax({
+						    type: "GET",
+						    url: "http://localhost:8080/ScreenTest/output/apptschedule.json",
+						    //url: "http://localhost:8080/ScreenTest/output/apptagents.json/"+serviceid_id,
+						    //data: "name=John&location=Boston",
+						    success: function(data){
+						      var obj= ko.utils.arrayMap(data,function(item){
+						    	   return new ApptSchedule(item.appointmentName,item.appointmentDate,item.agentId,item.serviceId,item.notes,item.apptSourceChannel,item.apptSourceDt,item.apptSourceClientName); 
+						       });
+						      apptschedule= obj;
+						      alert(ko.toJSON(apptschedule));
+						    }
+						});
+			    	 apptschedule= obj;
+				      alert(ko.toJSON("New"+apptschedule));
+			    	    }   	   
+			       });
 		},
-		bookAppt:function(data,event){
-
+		loadbookAppt:function(data,event){
 			apptViewModel.setTmpl('apptbook');
-			$( "#datepicker" ).datepicker();
+		  		alert("initalized");
+		
+		},
+		bookAppointment:function(data,event){
+			$.ajax({
+		          url: "http://localhost:8080/ScreenTest/output/apptschedule.json",
+		          type: "POST",
+		          contentType: "application/json",
+		          accept: "application/json",
+		          data:JSON.stringify($("#appointmentForm").serializeObject()),
+		          success: function (data, textStatus, jqXHR) {
+		          	alert("Appointment got Created Successfully");
+		          	//alert(data);
+		          	//var URL = data.URL + "?id="+data.AGENT_ID+"&JSESSIONID="+data.JSESSIONID;
+		          	//alert(URL)
+		          	//window.location = URL;
+		          },
+		          error: function (jqXHR, textStatus, errorThrown) {
+		          	//$("#messagediv").append("hai");
+		          	alert("error");
+		          	//alert(jqXHR);
+		          }
+		      });
 
+			alert("Form Submit");
+			apptViewModel.setTmpl('apptservices');
 		},
 		setTmpl: function ( template )
 		{
 			this.selectTmpl( template );
-			$( "#datepicker" ).datepicker();
-		},	
-
-};
-
-
+			  $( "#datepicker" ).datepicker();
+			},	
+			
+		};
 
 
-//ko.applyBindings(apptViewModel);
-//function ServicesSetModel( services ){
-
-//var servicesSet = ko.utils.arrayMap( services.setOfServices , function(service){
-//return new ServiceModel( service.serviceName, service.tasks);
-//});
-//this.setOfServices = ko.observableArray( servicesSet );
-//}
-
-//function ServiceModel(serviceName, tasksList){
-
-//this.serviceName = ko.observable( serviceName );
-//var tasks = ko.utils.arrayMap(tasksList, function( task ){
-//return new TaskModel(task.taskNm, task.time);
-//});
-//this.taskList = ko.observableArray( tasks );
